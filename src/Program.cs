e3a0d2a4e9a2508ad7cef2d4302d8d232cba7b18
Konstantin Konstantin <zetslief@protonmail.com>
2024-07-04 20:38:08 +0200

    Reading the same nettrace file using EventPipe.

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,33 @@
 using Nettrace;
+using Microsoft.Diagnostics.Tracing;
 
 var filePath = args[0];
-using var file = File.OpenRead(filePath);
-NettraceReader.Read(file);
+
+const bool useNettrace = false;
+if (useNettrace)
+{
+    UseNettrace(filePath);
+}
+else
+{
+    UseEventPipe(filePath);
+}
+
+static void UseNettrace(string filePath)
+{
+    using var file = File.OpenRead(filePath);
+    NettraceReader.Read(file);
+}
+
+static void UseEventPipe(string filePath)
+{
+    using var eventSource = new EventPipeEventSource(filePath);
+    int eventCount = 0; 
+    eventSource.AllEvents += (@event) =>
+    {
+        ++eventCount;
+        Console.WriteLine($"{eventCount}: {@event}"); 
+    };
+    eventSource.Process();
+    Console.WriteLine($"Total number of events: {eventCount}");
+}
