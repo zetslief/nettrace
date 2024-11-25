39c16cc9b9b5d586246a0068ac09356fa5ffeb7d
Konstantin Konstantin <zetslief@protonmail.com>
2024-11-25 22:12:48 +0100

    Add profileme app

---
+++ profileme/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics.Tracing;
+
+int index = 0;
+
+while (true)
+{
+    var time = DateTime.Now;
+    ProfileMe.Log.Tick(time);
+    Console.WriteLine($"{index++} : {time}");
+    await Task.Delay(TimeSpan.FromSeconds(1));
+}
+
+[EventSource(Name = "ProfileMe")]
+public class ProfileMe : EventSource
+{
+    public static ProfileMe Log { get; } = new();
+
+    [Event(1)]
+    public void Tick(DateTime time) => WriteEvent(1, time);
+}
