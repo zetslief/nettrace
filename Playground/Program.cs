2d260489f26ee1e9e55fa2bea4f3e6426e6882bc
Konstantin Konstantin <zetslief@protonmail.com>
2024-12-01 22:15:04 +0100

    Investigating how metadata is connected to events.

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -1,3 +1,36 @@
 ﻿using Nettrace;
+using System.Diagnostics;
+using static System.Console;
 
-NettraceReader.Read(File.OpenRead(args[0]));
+var filepath = args.Length > 0 ? args[0] : "perf.nettrace";
+
+var stopwatch = Stopwatch.StartNew();
+var trace = NettraceReader.Read(File.OpenRead(filepath));
+stopwatch.Stop();
+
+Dictionary<int, NettraceReader.MetadataHeader>  metadataStorage = [];
+
+foreach (var metadataBlock in trace.MetadataBlocks)
+{
+    foreach (var blob in metadataBlock.EventBlobs)
+    {
+        metadataStorage[blob.Payload.Header.MetaDataId] = blob.Payload.Header;
+    }
+}
+
+WriteLine("---------");
+
+foreach (var eventBlock in trace.EventBlocks)
+{
+    foreach (var blob in eventBlock.EventBlobs)
+    {
+        WriteLine($"{metadataStorage.ContainsKey(blob.MetadataId)} | {blob}");
+    }
+}
+
+foreach (var header in metadataStorage.Values)
+{
+    WriteLine(header);
+}
+
+WriteLine($"File was read in {stopwatch.Elapsed}");
