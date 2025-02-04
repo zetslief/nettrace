da42305c758635498173581c2f0370d42f148966
Konstantin Konstantin <zetslief@protonmail.com>
2025-02-04 21:20:02 +0100

    Render data when it is changes.

--- Explorer/Controls/TimespanControl.cs
+++ Explorer/Controls/TimespanControl.cs
@@ -19,12 +19,13 @@ public sealed class TimespanControl : Control
     private Position? pressed = null;
     private Position? current = null;
 
-    public static readonly DirectProperty<TimespanControl, Node?> DataNodeProperty = AvaloniaProperty.RegisterDirect<TimespanControl, Node?>(
-        nameof(DataNode),
-        owner => owner.dataNode,
-        SetDataNodeProperty,
-        defaultBindingMode: BindingMode.TwoWay);
-    
+    public static readonly DirectProperty<TimespanControl, Node?> DataNodeProperty =
+        AvaloniaProperty.RegisterDirect<TimespanControl, Node?>(
+            nameof(DataNode),
+            owner => owner.dataNode,
+            SetDataNodeProperty,
+            defaultBindingMode: BindingMode.TwoWay);
+
     public TimespanControl()
     {
         ClipToBounds = true;
@@ -38,10 +39,12 @@ public sealed class TimespanControl : Control
 
     public override void Render(DrawingContext context)
     {
-        var uiNode = new TreeNode((pressed, current) switch 
+        var uiNode = new TreeNode((pressed, current) switch
         {
-            (not null, not null) => [
-                new Rectangle(new(pressed.Value.X, pressed.Value.Y, current.Value.X, current.Value.Y), Color.FromArgb(90, 50, 100, 50)),
+            (not null, not null) =>
+            [
+                new Rectangle(new(pressed.Value.X, pressed.Value.Y, current.Value.X, current.Value.Y),
+                    Color.FromArgb(90, 50, 100, 50)),
                 new Point(pressed.Value, Colors.Yellow),
                 new Point(current.Value, Colors.Orange),
             ],
@@ -49,21 +52,21 @@ public sealed class TimespanControl : Control
             (null, not null) => [],
             (null, null) => []
         });
-        
+
         var uiCamera = new Camera2D(Position.Zero, Bounds.Into(), Bounds.Into());
-        
+
         Stack<(Camera2D, Node)> renderItems = [];
         renderItems.Push((uiCamera, uiNode));
-        
+
         if (dataNode is not null)
         {
             var dataCamera = new Camera2D(Position.Zero, viewport ?? dataBounds, Bounds.Into());
             renderItems.Push((dataCamera, dataNode));
         }
-        
-        context.Custom(new TimespanDrawOperation(Bounds, renderItems)); 
+
+        context.Custom(new TimespanDrawOperation(Bounds, renderItems));
     }
-    
+
     protected override void OnPointerReleased(PointerReleasedEventArgs e)
     {
         if (e.InitialPressMouseButton == MouseButton.Right)
@@ -73,7 +76,7 @@ public sealed class TimespanControl : Control
             Console.WriteLine($"Viewport is reset to data bounds: {dataBounds}");
             return;
         }
-        
+
         Debug.Assert(pressed is not null);
         current = e.GetPosition(this).Into();
         var currentCamera = new Camera2D(Position.Zero, viewport ?? dataBounds, Bounds.Into());
@@ -85,7 +88,7 @@ public sealed class TimespanControl : Control
         Dispatcher.UIThread.InvokeAsync(InvalidateVisual, DispatcherPriority.Background);
         base.OnPointerEntered(e);
     }
-    
+
     protected override void OnPointerPressed(PointerPressedEventArgs e)
     {
         pressed = e.GetPosition(this).Into();
@@ -100,38 +103,42 @@ public sealed class TimespanControl : Control
         base.OnPointerMoved(e);
         Dispatcher.UIThread.InvokeAsync(InvalidateVisual, DispatcherPriority.Input);
     }
-    
+
     private static void SetDataNodeProperty(TimespanControl owner, Node? value)
     {
         owner.dataNode = value;
         Console.WriteLine($"Data Node is updated: {value}");
-        
+
         if (value is not null)
         {
             owner.dataBounds = Measure(null, value) ?? new(0, 0, 1000, 100);
             owner.viewport = owner.dataBounds;
             Console.WriteLine($"Viewport is measured: {owner.viewport}");
         }
+        
+        Dispatcher.UIThread.InvokeAsync(owner.InvalidateVisual, DispatcherPriority.Input);
     }
-    
+
     private static Rect? Measure(Rect? current, Node item)
     {
         static Rect OuterRectangle(Rect a, Rect b) => new(
             Math.Min(a.Left, b.Left), Math.Min(a.Top, b.Top),
             Math.Max(a.Right, b.Right), Math.Max(a.Bottom, b.Bottom)
         );
-        
+
         static Rect OuterPosition(Rect a, Position p) => new(
             Math.Min(a.Left, p.X), Math.Min(a.Top, p.Y),
             Math.Max(a.Right, p.X), Math.Max(a.Bottom, p.Y)
         );
-        
+
         static Rect RectangleFromPosition(Position p) => new(p.X, p.Y, p.X, p.Y);
 
         return item switch
         {
             Rectangle rectangle => current is null ? rectangle.Rect : OuterRectangle(current.Value, rectangle.Rect),
-            Point point => current is null ? RectangleFromPosition(point.Position) : OuterPosition(current.Value, point.Position),
+            Point point => current is null
+                ? RectangleFromPosition(point.Position)
+                : OuterPosition(current.Value, point.Position),
             TreeNode stacked => stacked.Children
                 .Select(s => Measure(current, s))
                 .Aggregate(current, (l, r) => (l, r) switch
