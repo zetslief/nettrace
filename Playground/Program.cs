f1e5cc6555aff93c69550ee0124d1bbd8cf91058
Konstantin Konstantin <zetslief@protonmail.com>
2025-02-06 21:12:09 +0100

    Find socket file.

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+
+var process = Process.GetProcessesByName("profileMe").Single();
+
+Console.WriteLine($"Process {process.ProcessName} Id: {process.Id} started at {process.StartTime}");
+Console.WriteLine($"Start time UNIX: {((DateTimeOffset)process.StartTime).ToUnixTimeSeconds()}");
+
+var directory = Environment.GetEnvironmentVariable("TMP") ?? "/tmp";
+
+Debug.Assert(Directory.Exists(directory));
+
+Console.WriteLine($"Directory: {directory}");
+
+/*
+In order to ensure filename uniqueness, a disambiguation key is generated.
+On Mac and NetBSD, this is the process start time encoded as the number of seconds since UNIX epoch time.
+If /proc/$PID/stat is available (all other *nix platforms), then the process start time encoded as jiffies since boot time is used. 
+ */
+
+var file = Directory.GetFiles(directory, $"dotnet-diagnostic-{process.Id}-*").Single();
+Console.WriteLine($"File: {file}");
