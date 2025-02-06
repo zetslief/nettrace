a3e1984ca64989b05917c018cba7735ac7465e69
Konstantin Konstantin <zetslief@protonmail.com>
2025-02-06 22:38:46 +0100

    Starting session and reading response.

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System.Diagnostics;
 using System.Net.Sockets;
+using System.Text;
+using System.Runtime.InteropServices;
 using System.Threading;
 
 var process = Process.GetProcessesByName("profileMe").Single();
@@ -29,3 +31,67 @@ var endpoint = new UnixDomainSocketEndPoint(file);
 await socket.ConnectAsync(endpoint, cts.Token);
 
 Console.WriteLine($"Connected? {socket.Connected}");
+
+byte[] bytes = new byte[1000];
+var len = SetupCollectTracingCommand(bytes, [new("ProfileMe", 0, 0, string.Empty)]);
+
+var written = await socket.SendAsync(bytes[..len]);
+Console.WriteLine($"Wrote {written} bytes.");
+
+byte[] response = new byte[20 + 8];
+var read = await socket.ReceiveAsync(response);
+
+Console.WriteLine($"Read {read} bytes.");
+Console.WriteLine($"Session Id: {ReadSessionId(bytes)}");
+
+static int SetupCollectTracingCommand(Span<byte> buffer, IReadOnlyCollection<Provider> providers)
+{
+    static int SetupProvider(Span<byte> buffer, Provider provider)
+    {
+        BitConverter.TryWriteBytes(buffer[0..sizeof(ulong)], provider.Keywords);
+        var cursor = sizeof(ulong);
+        BitConverter.TryWriteBytes(buffer[cursor..(cursor + sizeof(ulong))], provider.LogLevel);
+        cursor += sizeof(ulong);
+        var providerName = Encoding.ASCII.GetBytes(provider.Name).AsSpan();
+        providerName.CopyTo(buffer[cursor..]);
+        cursor += providerName.Length;
+        // skip filter data for now.
+        return cursor;
+    }
+    
+    var magic = Encoding.ASCII.GetBytes("DOTNET_IPC_V1").AsSpan();
+    magic.CopyTo(buffer);
+    var size = 20;
+    byte eventPipeCommandSet = 0x02;
+    byte collectTracingCommandId = 0x02;
+    int eventPipeCommandSetIndex = 17;
+    int collectTracingCommandIdIndex = 18;
+    buffer[eventPipeCommandSetIndex] = eventPipeCommandSet;
+    buffer[collectTracingCommandIdIndex] = collectTracingCommandId;
+    
+    uint circularBufferMb = 1024;
+    BitConverter.TryWriteBytes(buffer[size..(size + sizeof(uint))], circularBufferMb);
+    size += sizeof(uint);
+    
+    uint format = 1;
+    BitConverter.TryWriteBytes(buffer[size..(size + sizeof(uint))], format);
+    size += sizeof(uint);
+    
+    uint providersCount = (uint)providers.Count;
+    BitConverter.TryWriteBytes(buffer[size..(size + sizeof(uint))], providersCount);
+    size += sizeof(uint);
+    
+    foreach (var provider in providers)
+        size += SetupProvider(buffer[size..], provider);
+    var sizeIndex = 15;
+    
+    BitConverter.TryWriteBytes(buffer[sizeIndex..(sizeIndex + sizeof(ushort))], size);
+    return size;
+}
+
+static ulong ReadSessionId(ReadOnlySpan<byte> response)
+{
+    return BitConverter.ToUInt64(response[20..]);
+}
+
+record Provider(string Name, ulong Keywords, ulong LogLevel, string FilterData);
