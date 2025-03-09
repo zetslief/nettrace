391a28ef4c4949579303b96642c704482c9c1b2e
Konstantin Konstantin <zetslief@protonmail.com>
2025-03-09 19:32:41 +0100

    Add tab control, nettrace reader view and refactored main view.

---
+++ Explorer/Controls/NettraceReaderView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Explorer.Controls;
+
+public partial class NettraceReaderView : UserControl
+{
+    public NettraceReaderView()
+    {
+        InitializeComponent();
+    }
+}
