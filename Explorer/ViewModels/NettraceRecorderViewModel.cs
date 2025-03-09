4a4fb0a597242d140b3b1c39203290bb20c5488c
Konstantin Konstantin <zetslief@protonmail.com>
2025-03-09 20:11:05 +0100

    Refresh buttons.

--- Explorer/ViewModels/NettraceRecorderViewModel.cs
+++ Explorer/ViewModels/NettraceRecorderViewModel.cs
@@ -1,22 +1,41 @@
 using ReactiveUI;
 using System.Linq;
 using System.Collections.Generic;
+using System.Windows.Input;
 using System.Diagnostics;
 
 namespace Explorer.ViewModels;
 
+public sealed class ProcessViewModel(Process process)
+{
+    public int Id { get; } = process.Id;
+    public string Name { get; } = process.ProcessName;
+}
+
 public class NettraceRecorderViewModel : ReactiveObject
 {
-    private IEnumerable<string>? processes;
+    private IEnumerable<ProcessViewModel>? processes;
 
     public NettraceRecorderViewModel()
     {
-        processes = Process.GetProcesses().Select(p => p.ToString()).ToArray();
+        RefreshCommand = ReactiveCommand.Create(Refresh);
+        
+        Refresh();
     }
 
-    public IEnumerable<string>? Processes
+    public ICommand RefreshCommand { get; }
+
+    public IEnumerable<ProcessViewModel>? Processes
     {
         get => processes;
         set => this.RaiseAndSetIfChanged(ref processes, value);
+    }
+
+    private void Refresh()
+    {
+        Processes = Process.GetProcesses()
+            .OrderByDescending(p => p.StartTime)
+            .Select(p => new ProcessViewModel(p))
+            .ToArray();
     }
 }
