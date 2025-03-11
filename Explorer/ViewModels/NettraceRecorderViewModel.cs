c2d9d28fd1f7a80eca059e00331e9f2b2e51ef3b
Konstantin Konstantin <zetslief@protonmail.com>
2025-03-11 21:44:42 +0100

    Collecting trace (not writing yet).

--- Explorer/ViewModels/NettraceRecorderViewModel.cs
+++ Explorer/ViewModels/NettraceRecorderViewModel.cs
@@ -2,6 +2,7 @@ using ReactiveUI;
 using System.Linq;
 using System;
 using System.IO;
+using System.Net.Sockets;
 using System.Collections.Generic;
 using System.Windows.Input;
 using System.Diagnostics;
@@ -60,10 +61,60 @@ public class NettraceRecorderViewModel : ReactiveObject
         set => this.RaiseAndSetIfChanged(ref eventProviders, value);
     }
 
-    private Task RecordAsync()
+    private async Task RecordAsync()
     {
+        static Provider ToEventProvider(EventProviderViewModel vm)
+            => new(vm.Name, ulong.MaxValue, 0, string.Empty);
+        
         Console.WriteLine($"{SelectedProcess?.SocketFilename}");
-        return Task.CompletedTask;
+        if (EventProviders is null || SelectedProcess is null)
+        {
+            return;
+        }
+        
+        var networkEndpoint = new UnixDomainSocketEndPoint(SelectedProcess.SocketFilename);
+        using var networkSocket = new Socket(AddressFamily.Unix, SocketType.Stream, ProtocolType.Unspecified);
+        await networkSocket.ConnectAsync(networkEndpoint).ConfigureAwait(false);
+        var (maybeError, maybeSessionId) = await DiagnosticIpc.TryCollectTracing(networkSocket, EventProviders.Select(ToEventProvider).ToArray()).ConfigureAwait(false);
+
+        if (maybeError is not null || maybeSessionId is null)
+        {
+            Console.WriteLine($"Failed to start collecting trace: {maybeError}");
+            return;
+        }
+        
+        var sessionId = maybeSessionId.Value;
+        Console.WriteLine($"Tracing start for session {sessionId}");
+
+        await Task.Delay(TimeSpan.FromSeconds(5)).ConfigureAwait(false);
+
+        Console.WriteLine($"Stopping...");
+        var stopEndpoint = new UnixDomainSocketEndPoint(SelectedProcess.SocketFilename);
+        using var stopSocket = new Socket(AddressFamily.Unix, SocketType.Stream, ProtocolType.Unspecified);
+        await stopSocket.ConnectAsync(stopEndpoint).ConfigureAwait(false);
+        Console.WriteLine($"Stop socket connected...");
+        var stopRequested = await DiagnosticIpc.TryRequestStopTracing(stopSocket, sessionId).ConfigureAwait(false);
+        if (!stopRequested)
+        {
+            Console.WriteLine($"Failed to request stop tracing.");
+            return;
+        }
+        
+        var buffer = new byte[1024];
+        var totalRead = 0;
+        var read = 1;
+        do
+        {
+            read = await networkSocket.ReceiveAsync(buffer).ConfigureAwait(false); 
+            totalRead += read;
+        }
+        while (read > 0);
+
+        Console.WriteLine($"Reading {totalRead} bytes...");
+        
+        Console.WriteLine($"Waiting for session to be stopped...");
+        var stopped = await DiagnosticIpc.TryWaitStopTracing(stopSocket, sessionId).ConfigureAwait(false);
+        Console.WriteLine($"Tracing stopped: {stopped}");
     }
 
     private void Refresh()
