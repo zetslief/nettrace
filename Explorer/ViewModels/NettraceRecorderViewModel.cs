94979c3a86265ada1713355012b6ce3cf16b1b66
Konstantin Konstantin <zetslief@protonmail.com>
2025-03-11 20:14:11 +0100

    Building and rendering ProcessViewModel

--- Explorer/ViewModels/NettraceRecorderViewModel.cs
+++ Explorer/ViewModels/NettraceRecorderViewModel.cs
@@ -8,10 +8,12 @@ using System.Diagnostics;
 
 namespace Explorer.ViewModels;
 
-public sealed class ProcessViewModel(Process process)
+public sealed class ProcessViewModel(string socketFileName, string? error, Process? process)
 {
-    public int Id { get; } = process.Id;
-    public string Name { get; } = process.ProcessName;
+    public int? Id { get; } = process?.Id;
+    public string? Name { get; } = process?.ProcessName;
+    public string SocketFilename { get; } = socketFileName;
+    public string? Error { get; } = error;
 }
 
 public class NettraceRecorderViewModel : ReactiveObject
@@ -37,19 +39,32 @@ public class NettraceRecorderViewModel : ReactiveObject
     {
         const string prefix = "dotnet-diagnostic-";
 
-        static Process? FileToProcess(string file)
+        static ProcessViewModel FileToProcess(string socketFile)
         {
-            file = Path.GetFileName(file);
-            var endIndex = file.IndexOf('-', prefix.Length);
-            Console.WriteLine($"{file} - {file[prefix.Length..endIndex]}");
-            return int.TryParse(file[prefix.Length..endIndex], out var id) ? Process.GetProcessById(id) : null;
+            var fileName = Path.GetFileName(socketFile);
+            var endIndex = fileName.IndexOf('-', prefix.Length);
+            var range = prefix.Length..endIndex;
+            var processId = fileName[range];
+            if (!int.TryParse(processId, out var id))
+            {
+                return new(socketFile, $"Failed to parse process id {processId} in {fileName}({range})", null);
+            }
+
+            try
+            {
+                var process = Process.GetProcessById(id);
+                return new(socketFile, null, process);
+            }
+            catch (ArgumentException e)
+            {
+                return new(socketFile, $"Failed to find process with id {id} ({fileName}). Error: {e}", null);
+            }
         }
 
         var directory = Environment.GetEnvironmentVariable("TMP") ?? "/tmp";
         var files = Directory.GetFiles(directory, $"{prefix}*");
         Processes = files
             .Select(FileToProcess)
-            .Select(p => new ProcessViewModel(p ?? throw new InvalidOperationException($"Failed to get process from file.")))
             .ToArray();
     }
 }
