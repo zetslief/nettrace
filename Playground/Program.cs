6ca97eec1cc2ef2b3f7efbe4c87b0849216e1620
Konstantin Konstantin <zetslief@protonmail.com>
2025-10-08 22:09:37 +0200

    Playground: render events per thread

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Nettrace;
 using Nettrace.PayloadParsers;
+using System.Collections.Immutable;
 
 using var stream = new MemoryStream(File.ReadAllBytes("./traces/tpleventsource_profileme.nettrace"));
 var file = NettraceReader.Read(stream);
@@ -14,48 +15,45 @@ foreach (var metadataBlock in file.MetadataBlocks)
     }
 }
 
-Dictionary<long, List<NettraceReader.EventBlob<NettraceReader.Event>>> threadStorage = [];
-foreach (var eventBlock in file.EventBlocks)
+var eventBlobs = file.EventBlocks.SelectMany(b => b.EventBlobs).ToImmutableArray();
+
+foreach (var eventBlob in eventBlobs)
 {
-    foreach (var eventBlob in eventBlock.EventBlobs)
-    {
-        if (threadStorage.TryGetValue(eventBlob.CaptureThreadId, out var threadBlobs))
-        {
-            threadBlobs.Add(eventBlob);
-        }
-        else
-        {
-            threadStorage[eventBlob.CaptureThreadId] = [eventBlob];
-        }
-    }
+    var metadata = metadataStorage[eventBlob.MetadataId].Payload;
+    if (metadata.Header.EventName.Length == 0) continue;
+    Console.Write($"{eventBlob.SequenceNumber} - Thread {eventBlob.ThreadId} - ");
+    PrintEventBlob(eventBlob, metadata);
 }
 
-foreach (var (thread, blobs) in threadStorage)
-{
-    Console.WriteLine($"Thread: {thread}");
-    foreach (var blob in blobs)
-    {
-        var metadata = metadataStorage[blob.MetadataId].Payload;
-        if (metadata.Header.EventName.Length == 0) continue;
-        ReadOnlySpan<byte> payloadBytes = blob.Payload.Bytes.Span;
-        IEvent @event = metadata.Header.EventName switch
-        {
-            var name when name == NewId.Name => TplParser.ParseNewId(payloadBytes),
-            var name when name == TraceSynchronousWorkBegin.Name => TplParser.ParseTraceSynchronousWorkBegin(payloadBytes),
-            var name when name == TraceSynchronousWorkEnd.Name => TplParser.ParseTraceSynchronousWorkEnd(payloadBytes),
-            var name when name == TaskWaitContinuationStarted.Name => TplParser.ParseTaskWaitContinuationStarted(payloadBytes),
-            var name when name == TraceOperationEnd.Name => TplParser.ParseTraceOperationEnd(payloadBytes),
-            var name when name == TraceOperationBegin.Name => TplParser.ParseTraceOperationBegin(payloadBytes),
-            var name when name == TaskWaitContinuationComplete.Name => TplParser.ParseTaskWaitContinuationComplete(payloadBytes),
-            var name when name == TaskWaitEnd.Name => TplParser.ParseTaskWaitEnd(payloadBytes),
-            var name when name == TaskWaitBegin.Name => TplParser.ParseTaskWaitBegin(payloadBytes),
-            var name when name == AwaitTaskContinuationScheduled.Name => TplParser.ParseAwaitTaskContinuation(payloadBytes),
-            var name when name == TaskScheduled.Name => TplParser.ParseTaskScheduled(payloadBytes),
-            var name when name == TraceOperationRelation.Name => TplParser.ParseTraceOperationRelation(payloadBytes),
-            var name when name == ProcessInfo.Name => ProcessInfoParser.ParseProcessInfo(payloadBytes),
-            var other => throw new NotImplementedException($"Blob parsing is not implemented for \n\t{metadata.Header.EventName} {blob.SequenceNumber} {metadata.Payload}")
-        };
-        Console.WriteLine(@event);
-    }
-}
+return;
 
+static void PrintEventBlob(
+    NettraceReader.EventBlob<NettraceReader.Event> blob,
+    NettraceReader.MetadataEvent metadata)
+{
+    if (metadata.Header.EventName.Length == 0)
+    {
+        Console.WriteLine($"No event name in {metadata}");
+        return;
+    }
+
+    ReadOnlySpan<byte> payloadBytes = blob.Payload.Bytes.Span;
+    IEvent @event = metadata.Header.EventName switch
+    {
+        var name when name == NewId.Name => TplParser.ParseNewId(payloadBytes),
+        var name when name == TraceSynchronousWorkBegin.Name => TplParser.ParseTraceSynchronousWorkBegin(payloadBytes),
+        var name when name == TraceSynchronousWorkEnd.Name => TplParser.ParseTraceSynchronousWorkEnd(payloadBytes),
+        var name when name == TaskWaitContinuationStarted.Name => TplParser.ParseTaskWaitContinuationStarted(payloadBytes),
+        var name when name == TraceOperationEnd.Name => TplParser.ParseTraceOperationEnd(payloadBytes),
+        var name when name == TraceOperationBegin.Name => TplParser.ParseTraceOperationBegin(payloadBytes),
+        var name when name == TaskWaitContinuationComplete.Name => TplParser.ParseTaskWaitContinuationComplete(payloadBytes),
+        var name when name == TaskWaitEnd.Name => TplParser.ParseTaskWaitEnd(payloadBytes),
+        var name when name == TaskWaitBegin.Name => TplParser.ParseTaskWaitBegin(payloadBytes),
+        var name when name == AwaitTaskContinuationScheduled.Name => TplParser.ParseAwaitTaskContinuation(payloadBytes),
+        var name when name == TaskScheduled.Name => TplParser.ParseTaskScheduled(payloadBytes),
+        var name when name == TraceOperationRelation.Name => TplParser.ParseTraceOperationRelation(payloadBytes),
+        var name when name == ProcessInfo.Name => ProcessInfoParser.ParseProcessInfo(payloadBytes),
+        var other => throw new NotImplementedException($"Blob parsing is not implemented for \n\t{metadata.Header.EventName} {blob.SequenceNumber} {metadata.Payload}")
+    };
+    Console.WriteLine(@event);
+}
