d5fb40c0e9225e95c0df580e2a7bcdb29f1b7dc5
Konstantin Konstantin <zetslief@protonmail.com>
2025-12-08 22:52:46 +0100

    Playground: measuring task

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -5,10 +5,12 @@ using Nettrace;
 using Nettrace.HighLevel;
 using Nettrace.PayloadParsers;
 
+using static Nettrace.NettraceReader;
+
 using var stream = new MemoryStream(File.ReadAllBytes("/home/cube/progs/nettrace/traces/tpleventsource_profileme.nettrace"));
 var file = NettraceReader.Read(stream);
 
-Dictionary<int, NettraceReader.EventBlob<NettraceReader.MetadataEvent>> metadataStorage = [];
+Dictionary<int, EventBlob<MetadataEvent>> metadataStorage = [];
 
 foreach (var metadataBlock in file.MetadataBlocks)
 {
@@ -20,33 +22,35 @@ foreach (var metadataBlock in file.MetadataBlocks)
 
 var events = file.EventBlocks
     .SelectMany(b => b.EventBlobs)
-    .Select(b => (object)NettraceEventParser.ProcessEvent(metadataStorage[b.MetadataId].Payload, b))
-    .ToImmutableArray();
-var stacks = file.StackBlocks.Select(s => s.BuildStackInfos(file.Trace.PointerSize))
-    .SelectMany(s => s)
+    .Select(b => (b, (object)NettraceEventParser.ProcessEvent(metadataStorage[b.MetadataId].Payload, b)))
     .ToImmutableArray();
 
-Dictionary<ulong, MethodDCEndVerbose> addressMethodMap = [];
-var methods = events
-    .Where(e => e is MethodDCEndVerbose)
-    .Cast<MethodDCEndVerbose>()
-    .OrderBy(m => m.MethodStartAddress)
-    .ToImmutableArray();
-foreach (var stack in stacks)
+Dictionary<int, (EventBlob<Event>, TraceOperationBegin)> traceBegins = [];
+Dictionary<int, (EventBlob<Event>, TraceOperationEnd)> traceEnds = [];
+
+foreach (var (blob, @event) in events)
 {
-    var error = StackHelpers.TryBuildAddressMethodMap(stack, methods, addressMethodMap);
-    if (error is not null) throw new InvalidOperationException($"{error}");
-    DumpStack(stack, addressMethodMap);
+    if (@event is TraceOperationBegin traceBegin)
+    {
+        traceBegins[traceBegin.TaskId] = (blob, traceBegin);
+    }
+    if (@event is TraceOperationEnd traceEnd)
+    {
+        traceEnds[traceEnd.TaskId] = (blob, traceEnd);
+    }
 }
 
-static void DumpStack(StackInfo stack, IReadOnlyDictionary<ulong, MethodDCEndVerbose> stackMethodMap)
+foreach (var (taskId, beginTuple) in traceBegins)
 {
-    Console.WriteLine($"Stack {stack.Id} Height: {stack.Addresses.Length}");
-    foreach (var address in stack.Addresses)
+    var (beginBlob, begin) = beginTuple;
+    if (traceEnds.TryGetValue(taskId, out var endTuple))
     {
-        if (stackMethodMap.TryGetValue(address, out var method))
-            Console.WriteLine($"\t0x{address:x} {method.MethodSignature} {method.MethodNamespace} {method.MethodName}");
-        else
-            Console.WriteLine($"\t0x{address:x} <Unknown>");
+        var (endBlob, end) = endTuple;
+        var duration = endBlob.TimeStamp - beginBlob.TimeStamp;
+        Console.WriteLine($"TaskId {taskId} {begin.OperationName} Duration: {duration}");
+    }
+    else
+    {
+        Console.WriteLine($"WARNING: failed to find end for {taskId}.");
     }
 }
