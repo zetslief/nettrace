9c9cf5fe4a2d63ea8203d5b0fd7c78638bfb9ebd
Konstantin Konstantin <zetslief@protonmail.com>
2025-12-08 22:27:10 +0100

    profileme: simpler example

--- profileme/Program.cs
+++ profileme/Program.cs
@@ -1,37 +1,17 @@
 ﻿using System.Diagnostics.Tracing;
 
-TplEventListener? listener = null;
-if (args.Length > 0)
-    listener = new TplEventListener();
-
-try
+while (true)
 {
-    while (true)
-    {
-        await Task.Delay(TimeSpan.FromSeconds(5));
-        await Task.Yield();
-    }
-}
-finally
-{
-    listener?.Dispose();
+    await ShortRunningTask();
+    await LongRunningTask();
 }
 
-
-public sealed class TplEventListener : EventListener
+static async Task ShortRunningTask()
 {
-    protected override void OnEventSourceCreated(EventSource eventSource)
-    {
-        if (eventSource.Name == "System.Threading.Tasks.TplEventSource")
-            EnableEvents(eventSource, EventLevel.Verbose, EventKeywords.All);
-    }
+    await Task.Delay(TimeSpan.FromMilliseconds(100));
+}
 
-    protected override void OnEventWritten(EventWrittenEventArgs eventData)
-    {
-        Console.WriteLine($"event: {eventData.EventSource.Name}_{eventData.EventName}");
-        if (eventData.PayloadNames is not null)
-            Console.WriteLine($"\t Names   : {string.Join(',', eventData.PayloadNames)}");
-        if (eventData.Payload is not null)
-            Console.WriteLine($"\t Payloads: {string.Join(',', eventData.Payload)}");
-    }
+static async Task LongRunningTask()
+{
+    await Task.Delay(TimeSpan.FromSeconds(1));
 }
